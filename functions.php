0386558b98de3f01bfa2ce481d25ae8d67b9be74
admin <alex.darmos@gmail.com>
2021-06-23 22:52:06 -0400

    started customizing product page layout

--- functions.php
+++ functions.php
@@ -1,2 +1,29 @@
 <?php
 
+
+
+echo 'test ' . ( is_product() );
+// if ( is_product() ) {
+    remove_action('woocommerce_after_single_product_summary','woocommerce_output_product_data_tabs',10);
+
+    function tawards_data_tabs() {
+        $product_tabs = apply_filters( 'woocommerce_product_tabs', array() );
+
+        if ( ! empty( $product_tabs ) ) : ?>
+
+            <div class="tawards-tabs-container">
+                <div class="tawards-tab-title-container">
+                    <?php foreach( $product_tabs as $key => $product_tab ) : ?>
+                        <div class="tab-title">
+                            <a><?php echo wp_kses_post( $product_tab['title'] ); ?></a>
+                        </div>
+                    <?php endforeach; ?>
+                </div>
+            </div>
+
+            <?php endif; 
+    }
+
+    add_action('woocommerce_after_single_product_summary', 'tawards_data_tabs', 10);
+
+// }
